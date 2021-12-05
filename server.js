ae35d0898db8856261cc2445478fe3c6374b1f3c
BuildAllTogether <danielip@buffalo.edu>
2021-12-04 20:24:42 -0500

    Basic Page with button sending to login page

--- server.js
+++ server.js
@@ -2,22 +2,26 @@
 //Install express. npm install express --save
 
 const express = require('express');//Loads the module
+const path = require("path");
 const app = express();//Initiates it or something, maybe runs it.
 
 //This is where we handle each of our routes
 //req = request 'object', (request from the server)
 //res = response 'object', (what we are sending back)
+app.use(express.static(__dirname + '/public'));
 
-//app,get == GET requests, app.post == POST requests
 app.get('/', (req,res) => {
-    res.send("Hello World");
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html')
 })
 
 
-
+const port = 8000
 
 //This starts the app and makes it listen on port 8000
-const server = app.listen(8000, () =>{
-    console.log(`Server listening on port ${server.address().port}`);
-})
-
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`);
+});
